4cae195caa032a95540a3b3fd939ffb249e1c2bd
Carlos Rodriguez <crbotello08@gmail.com>
2024-09-30 10:30:32 +0200

    terminar mostrar php y quitar checkbox en formulario

--- mostrar.php
+++ mostrar.php
@@ -2,13 +2,13 @@
     echo 'Nombre: '.$_GET["nombre"];
     echo 'Apellido: '.$_GET["apel"];
     echo 'Fecha: '.$_GET["fecha"];
-    echo 'Sexo: '.$_GET["sexo"];
+    echo 'Sexo: '.$_GET["button"];
     echo 'Opinión personal: '.$_GET["textearea"];
-    echo 'Sentimientoss: '.$_GET["checkbox"];
+    echo 'Desastre más llamativo: '.$_GET["opciones"];
 
     if (isset($_GET["terminos"])) {
         if (empty($_GET["terminos"])) {
-            // PONER TEXTO AQUI
+            echo "Terminos aceptados";
         }
     }
 ?>
