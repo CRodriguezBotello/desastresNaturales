18869065f0a0cce23c5c0d7aa816ac22580856aa
Carlos Rodriguez <crbotello08@gmail.com>
2024-09-30 11:30:42 +0200

    recuperar textarea

--- mostrar.php
+++ mostrar.php
@@ -3,6 +3,7 @@
     echo '<br/><br/>Apellido: '.$_GET["apel"];
     echo '<br/><br/>Fecha: '.$_GET["fecha"];
     echo '<br/><br/>Sexo: '.$_GET["button"];
+    // echo '<br/><br/>Opinión personal: '.$_GET["textarea"];
     echo '<br/><br/>Desastre más llamativo: '.$_GET["opciones"];
 
     if (isset($_GET["terminos"])) {
