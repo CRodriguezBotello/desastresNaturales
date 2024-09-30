16e172fca730a8a2fa178f1eaeaa50d14b5cf3d0
Carlos Rodriguez <crbotello08@gmail.com>
2024-09-30 10:35:50 +0200

    quitar textarea y acabar formulario

--- mostrar.php
+++ mostrar.php
@@ -1,10 +1,10 @@
 <?php
+require_once "formulario.html";
     echo 'Nombre: '.$_GET["nombre"];
-    echo 'Apellido: '.$_GET["apel"];
-    echo 'Fecha: '.$_GET["fecha"];
-    echo 'Sexo: '.$_GET["button"];
-    echo 'Opinión personal: '.$_GET["textearea"];
-    echo 'Desastre más llamativo: '.$_GET["opciones"];
+    echo '<br/><br/>Apellido: '.$_GET["apel"];
+    echo '<br/><br/>Fecha: '.$_GET["fecha"];
+    echo '<br/><br/>Sexo: '.$_GET["button"];
+    echo '<br/><br/>Desastre más llamativo: '.$_GET["opciones"];
 
     if (isset($_GET["terminos"])) {
         if (empty($_GET["terminos"])) {
